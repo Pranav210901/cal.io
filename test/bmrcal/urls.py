08fa7bbc84cd65a9a833f4012f3cf65cc985f6e5
Pranav Pokhrel <121820390+Pranav210901@users.noreply.github.com>
2024-09-30 00:31:36 +0400

    Delete test directory

--- test/bmrcal/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-#importing views from the global scope of count
-from . import views
-urlpatterns = [
-    path('', views.bmr_cal, name='bmr_cal'),
-]
--- test/count/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-#importing views from the global scope of count
-from . import views
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('food_cal/', views.food_cal, name='food_cal'),
-]
